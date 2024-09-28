cd945e05cb7d67e39758e7f0098654a2099403ce
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2024-09-28 14:46:50 +0300

    init db

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"crud/persistence"
+	"fmt"
+	"log"
+
+	"github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
+)
 
 func main() {
 	fmt.Println("Server is up and running...")
+
+	// connect to ArangoDB
+	conn, err := http.NewConnection(
+		http.ConnectionConfig{
+			Endpoints: []string{"http://localhost:8529"},
+		},
+	)
+	if err != nil {
+		log.Fatalf("Failed to create HTTP connection: %v", err)
+	}
+	client, err := driver.NewClient(
+		driver.ClientConfig{
+			Connection:     conn,
+			Authentication: driver.BasicAuthentication("root", "rootpassword"),
+		},
+	)
+	if err != nil {
+		log.Fatalf("Failed to create the client: %v", err)
+	}
+
+	// Create database
+	clientRepo := persistence.NewClientRepository(client)
+	db, err := clientRepo.GetOrCreateDB()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	// Create collection
+	dbRepo := persistence.NewDatabaseRepository(db)
+	collection, err := dbRepo.GetOrCreateCollection()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	// Bulk create 100 documents in the collection
+	collectionRepo := persistence.NewCollectionRepository(collection)
+	if collectionRepo.IsEmpty() {
+		_, err = collectionRepo.BulkCreate(100)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+	}
 }
---
+++ persistence/initdb.go
@@ -0,0 +1,112 @@
+package persistence
+
+import (
+	"context"
+	"crud/domain"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/arangodb/go-driver"
+)
+
+type ClientRepository struct {
+	client driver.Client
+}
+
+func NewClientRepository(client driver.Client) *ClientRepository {
+	return &ClientRepository{client: client}
+}
+
+type DatabaseRepository struct {
+	db driver.Database
+}
+
+func NewDatabaseRepository(db driver.Database) *DatabaseRepository {
+	return &DatabaseRepository{db: db}
+}
+
+type CollectionRepository struct {
+	collection driver.Collection
+}
+
+func NewCollectionRepository(collection driver.Collection) *CollectionRepository {
+	return &CollectionRepository{collection: collection}
+}
+
+func (cr ClientRepository) GetOrCreateDB() (driver.Database, error) {
+	db_exists, err := cr.client.DatabaseExists(context.Background(), "crud")
+	if err != nil {
+		log.Fatalf("Failed to check if database exists")
+	}
+	var db driver.Database
+	if !db_exists {
+		db, err = cr.client.CreateDatabase(context.Background(), "crud", nil)
+		if err != nil {
+			log.Fatalf("Failed to create database")
+			return nil, err
+		}
+	} else {
+		db, err = cr.client.Database(context.Background(), "crud")
+		if err != nil {
+			log.Fatalf("Failed to open database")
+			return nil, err
+		}
+	}
+	return db, nil
+}
+
+func (dr DatabaseRepository) GetOrCreateCollection() (driver.Collection, error) {
+	collection, err := dr.db.Collection(context.Background(), "products")
+	if driver.IsNotFoundGeneral(err) {
+		collection, err = dr.db.CreateCollection(context.Background(), "products", nil)
+		if err != nil {
+			log.Fatalf("Failed to create collection")
+			return nil, err
+		}
+	} else if err != nil {
+		return nil, err
+	}
+	return collection, nil
+}
+
+func (cr CollectionRepository) BulkCreate(n int64) ([]domain.Product, error) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Buld create 100 random products in the collection `products`
+	qualities := []string{"A", "B", "C", "D", "E"}
+	categories := []string{"Category I", "Category II", "Category III"}
+	products := make([]domain.Product, n)
+	for i := 0; i < 100; i++ {
+		products[i] = domain.Product{
+			Name:     fmt.Sprintf("Name%d", i+1),
+			Price:    fmt.Sprintf("%d", rand.Intn(1000-99)+99),
+			Category: categories[r.Intn(len(categories))],
+			Quality:  qualities[r.Intn(len(qualities))],
+		}
+	}
+
+	// Insert products into the collection
+	var dbProducts []domain.Product
+	for _, product := range products {
+		meta, err := cr.collection.CreateDocument(context.Background(), product)
+		if err != nil {
+			log.Fatalf("An error occurred during bulk creation")
+			return nil, err
+		}
+		product.Key = meta.Key
+		product.ID = meta.ID.String()
+		dbProducts = append(dbProducts, product)
+	}
+
+	return dbProducts, nil
+}
+
+func (cr CollectionRepository) IsEmpty() bool {
+	numOfDocs, err := cr.collection.Count(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get document count")
+	}
+	return numOfDocs == 0
+}
