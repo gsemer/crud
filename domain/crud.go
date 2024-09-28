00abf5682ae2b477e99821a3d0107a8409fbd6dd
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2024-09-28 14:47:31 +0300

    product structure

--- domain/crud.go
+++ domain/crud.go
@@ -1,7 +1,10 @@
 package domain
 
 type Product struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Price string `json:"price"`
+	ID       string `json:"_id,omitempty"`
+	Key      string `json:"_key,omitempty"`
+	Name     string `json:"name"`
+	Price    string `json:"price"`
+	Category string `json:"category"`
+	Quality  string `json:"quality"`
 }
