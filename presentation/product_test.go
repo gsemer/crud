36c0fae5eae618d14596f9187d11bc9dcac0a693
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2024-09-29 12:56:37 +0300

    get products success test

---
+++ presentation/product_test.go
@@ -0,0 +1,69 @@
+package presentation
+
+import (
+	"crud/app/fakes"
+	"crud/domain"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestGetProducts(t *testing.T) {
+	ps := &fakes.FakeProductService{}
+	products := []domain.Product{
+		domain.Product{
+			ID:       "123e4567-e89b-12d3-a456-426614174000",
+			Key:      "1234",
+			Name:     "Product I",
+			Price:    "12.5",
+			Category: "Caterory II",
+			Quality:  "D",
+		},
+		domain.Product{
+			ID:       "123e5567-e89b-12d3-a456-426614174003",
+			Key:      "1235",
+			Name:     "Product II",
+			Price:    "124.7",
+			Category: "Caterory I",
+			Quality:  "C",
+		},
+	}
+	ps.GetProductsReturns(products, nil)
+
+	myHandlerFunc := ProductHandler{ps: ps}
+
+	request := httptest.NewRequest(http.MethodGet, "/products", nil)
+	response := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/products", myHandlerFunc.GetProducts)
+	router.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Status code is not %v, but is %v", http.StatusOK, response.Code)
+		return
+	}
+
+	productsList, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var result []domain.Product
+	err = json.Unmarshal([]byte(productsList), &result)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(result, products) {
+		t.Error("[ERROR] Not the expected output")
+		return
+	}
+}
